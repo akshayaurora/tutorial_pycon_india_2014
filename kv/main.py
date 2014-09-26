9ee3db3796dde66ae0d39f8deea1aec3cdfb236e
akshayaurora <akshayaurora@gmail.com>
2014-09-26 08:02:32 +0530

    minor changes

---
+++ kv/main.py
@@ -0,0 +1,13 @@
+'''Hello World Example in kivy
+'''
+from kivy.app import App
+
+
+class Main(App):
+    '''Just a Hello Worldapp
+    '''
+    pass
+
+if __name__ == '__main__':
+    Main().run()
+
---
+++ plyer/main.py
@@ -0,0 +1,19 @@
+'''Plyer test
+'''
+from kivy.app import App
+from plyer import battery
+from plyer import notification
+from plyer import tts
+
+class Main(App):
+
+    def get_battery(self, but):
+        but.text = repr(battery.status)
+
+    def speak(self):
+        tts.speak('Plyer is sexy!')
+
+    def notify(self):
+        notification.notify('Hello World')
+
+Main().run()
